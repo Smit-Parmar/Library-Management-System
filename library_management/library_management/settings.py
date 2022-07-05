6489acd36fe554bd70eca0ea9098b24af030fab3
smitparmar33 <smit.p@yudiz.com>
2022-07-05 19:20:11 +0530

    integrate login api with jquery

--- library_management/library_management/settings.py
+++ library_management/library_management/settings.py
@@ -76,6 +76,9 @@ TEMPLATES = [
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.TokenAuthentication',
+        'rest_framework.authentication.BasicAuthentication',
     ),'DEFAULT_FILTER_BACKENDS': (
         'django_filters.rest_framework.DjangoFilterBackend',
     ),
---
+++ library_management/static/js/signin.js
@@ -0,0 +1,74 @@
+$(() => {
+    console.log("Boom!");
+    const token = window.localStorage.getItem("Authorization");
+    if(token) {
+        alert("User is already logged in!");
+        window.location.href = "/";
+    }
+});
+
+$("#btn-login").on('click', loginUser);
+
+function loginUser() {
+    console.log("Login Clicked!");
+
+    const sEmailID = $("#email").val();
+    const sPassword = $("#password").val();
+
+    console.log({ sEmailID });
+    console.log({ sPassword });
+
+    if (!sEmailID) {
+        alert("Please enter email ID!")
+        return;
+    }
+
+    if (!sPassword) {
+        alert("Please enter Password!")
+        return;
+    }
+
+    // TODO: validate the data
+
+    $.ajax({
+        type: "POST",
+        url: "http://127.0.0.1:8000/api/token/",
+        data: {
+            email: sEmailID,
+            password: sPassword
+        },
+        dataType: 'json',
+        success: function (result, status, xhr) {
+            alert("User Logged In Successfully");
+            console.log(result);
+            console.log(status);
+            console.log(xhr);
+
+            window.localStorage.setItem("Authorization", xhr.responseJSON.access)
+
+            window.location.href = '/';
+
+        },
+        error: function (xhr, status, error) {
+            console.log(xhr);
+            console.log(status);
+            console.log(error);
+            alert(xhr.responseJSON.detail);
+        }
+    })
+    // $.ajax({
+    //     type: "GET",
+    //     url: "http://127.0.0.1:8000/api/v1/book/",
+    //     success: function (result, status, xhr) {
+    //         console.log(result);
+    //         console.log(status);
+    //         console.log(xhr);
+    //     },
+    //     error: function (xhr, status, error) {
+    //         console.log(xhr);
+    //         console.log(status);
+    //         console.log(error);
+    //     }
+    // })
+
+}
