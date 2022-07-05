e66c9eb51a25e8fe2757d9cedc6e70e30b038f35
smitparmar33 <smit.p@yudiz.com>
2022-07-05 17:53:39 +0530

    added signing template

--- library_management/frontapp/urls.py
+++ library_management/frontapp/urls.py
@@ -6,4 +6,5 @@ from . import views
 
 urlpatterns = [
     path("",views.homeView,name="home"),
+    path("login-signup",views.loginSignUp,name="login-signup"),
 ]
--- library_management/frontapp/views.py
+++ library_management/frontapp/views.py
@@ -2,11 +2,9 @@ from django.shortcuts import render
   
 # create a function
 def homeView(request):
-    # create a dictionary to pass
-    # data to the template
-    context ={
-        "data":"Gfg is the best",
-        "list":[1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-    }
-    # return response with template and context
-    return render(request, "index.html", context)
+
+    return render(request, "index.html")
+
+def loginSignUp(request):
+
+    return render(request, "signin.html")
